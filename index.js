c204260aa508785c764f4a85a0f70374be9342cd
Nick Casciola <s0948644@monmouth.edu>
2017-10-12 20:16:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function divide(a,b) {
 }
 
 function inc(n) {
-   a = n + 1;
-   return a;
+   n + 1;
+   return n;
 }
 
 function dec(n) {
-  a = n - 1;
-  return a;
+  n - 1;
+  return n;
 }
 
 function makeInt(n) {
